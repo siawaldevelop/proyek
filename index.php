3cad4afb5719ce7fe53b77caba55e99f8cb08d4a
Rico Maykel Erawanto <rikomaykel01@gmai.com>
2022-10-01 17:11:16 +0700

    memperbagus tampilan form

--- index.php
+++ index.php
@@ -20,17 +20,17 @@
                         <!-- Nested Row within Card Body -->
                         <div class="row justify-content-center">
 
-                            <div class="col-lg-8">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">MASUKKAN NAMA LENGKAP UNTUK MEMBUKA UNDANGAN!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">MASUKKAN NAMA</h1>
                                     </div>
                                     <form action="core/welcome.php" method="post">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="exampleInputText" name="nama" placeholder="Nama Lengkap">
                                         </div>
 
-                                        <input type="submit" value="Next" name="btn btn-primary"></input>
+                                        <input type="submit" value="Berikutnya" name="btn btn-primary"></input>
 
                                     </form>
 
