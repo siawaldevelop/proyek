62967bc681258ce2072290fc7d99b0b137ae8032
siawaldevelop <114715157+siawaldevelop@users.noreply.github.com>
2022-10-02 17:39:29 +0700

    mensetting menu welcome

--- core/welcome.php
+++ core/welcome.php
@@ -10,10 +10,26 @@
 </head>
 
 <body>
-    <h2>INI ADALAH HALAMAN WELCOME UNDANGAN</h2>
-    <?php
-    echo "Kpd Saudara: " . $_POST['nama']; ?><br>
+    <!-- <h2>INI ADALAH HALAMAN WELCOME UNDANGAN</h2>
+<br>
     <a href="main.php"><button type="button" class="btn btn-danger">Buka Undangan</button></a>
+-->
+    <section id="main" class="main">
+        <div class="card_welcome">
+            <div class="card_main">
+                <h2>Natal Permata GBKP Rg Diski</h2>
+                <br>
+            </div>
+            <div class="card_main">
+                <h3>Selamat Datang</h3>
+                <h4> <?php
+                        echo "Kpd Saudara: " . $_POST['nama']; ?></h4>
+
+                <a href="main.php"><button type="button" class="btn1">Buka Undangan</button></a>
+            </div>
+        </div>
+    </section>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
